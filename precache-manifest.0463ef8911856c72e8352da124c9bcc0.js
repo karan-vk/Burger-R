7794b37eee7a90ac27eea8dfc48885c62680246a
Karan V <karanvknarayanan@gmail.com>
2020-06-14 22:50:12 +0530

    Updates

--- precache-manifest.0463ef8911856c72e8352da124c9bcc0.js
+++ precache-manifest.0463ef8911856c72e8352da124c9bcc0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eaabf03e49b6f964eb47499f31c25e8b",
+    "revision": "627aa9a7b4d2c9cc1fa20d457f333ddd",
     "url": "/React-Burger/index.html"
   },
   {
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Burger/static/css/3.f3d15e72.chunk.css"
   },
   {
-    "revision": "afd84320c6a6cfe5ae12",
+    "revision": "df7f0152683ae96ed7fc",
     "url": "/React-Burger/static/css/4.dd290c88.chunk.css"
   },
   {
-    "revision": "b965e5873051d997f012",
+    "revision": "710c164cd376b157b778",
     "url": "/React-Burger/static/css/main.91705067.chunk.css"
   },
   {
-    "revision": "c27b8d5a1a0c46c4b561",
-    "url": "/React-Burger/static/js/2.ab863cee.chunk.js"
+    "revision": "cfa58da3f523966fa505",
+    "url": "/React-Burger/static/js/2.2618d836.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/React-Burger/static/js/2.ab863cee.chunk.js.LICENSE.txt"
+    "url": "/React-Burger/static/js/2.2618d836.chunk.js.LICENSE.txt"
   },
   {
     "revision": "fbee7c3df2ac4d65fe65",
     "url": "/React-Burger/static/js/3.462858b2.chunk.js"
   },
   {
-    "revision": "afd84320c6a6cfe5ae12",
-    "url": "/React-Burger/static/js/4.4b586092.chunk.js"
+    "revision": "df7f0152683ae96ed7fc",
+    "url": "/React-Burger/static/js/4.2d8a66bf.chunk.js"
   },
   {
-    "revision": "b965e5873051d997f012",
-    "url": "/React-Burger/static/js/main.8298e2b1.chunk.js"
+    "revision": "710c164cd376b157b778",
+    "url": "/React-Burger/static/js/main.70b527d6.chunk.js"
   },
   {
-    "revision": "ba82f7ee5538145cb4f4",
-    "url": "/React-Burger/static/js/runtime-main.15c7980e.js"
+    "revision": "0673c7d97c012f60b1a7",
+    "url": "/React-Burger/static/js/runtime-main.5c77e38a.js"
   },
   {
     "revision": "b8503d262bffbfb7c67fd6762963e7d1",
